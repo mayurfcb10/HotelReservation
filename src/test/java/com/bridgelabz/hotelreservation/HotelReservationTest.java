2802e817a670be345b94efda2f10bd8e83c889c1
Mayur <mayurzope555@gmail.com>
2020-10-13 17:56:14 +0530

    [Mayur]Added: Find Cheapest Hotel for a given date range based on weekdays and weekend days

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -1,28 +1,31 @@
 package com.bridgelabz.hotelreservation;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class HotelReservationTest 
-{
+public class HotelReservationTest {
 	static HotelReservation hotelReservation;
+
 	@BeforeClass
 	public static void initialize() {
 		hotelReservation = new HotelReservation();
 	}
-	
-	@Test
-	public void shouldPrintWelcomeMessage()  {
-		HotelReservation hotelReservation = new HotelReservation();
+
+	@Before
+	public void shouldPrintWelcomeMessage() {
 		hotelReservation.printWelcomeMessage();
 	}
-	
-	 @Test
-     public void givenDate_shouldReturnLakeWood()throws Exception {
-         HotelReservation hotelReservation = new HotelReservation();
-         hotelReservation.addHotelDetails();
-         String cheapestHotel = hotelReservation.calculateCheapestHotelAndRate("10Sep2020", "11Sep2020");
-         Assert.assertEquals("Lakewood", cheapestHotel);
-     }
+
+	@Test
+	public void givenDate_shouldReturn_CheapestHotelNameAndRate() throws Exception {
+		hotelReservation.addHotelDetails();
+		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-10", "2020-09-11");
+		Object[] hotelName = hotelNameList.toArray();
+		Object[] arrayExpectedOutput = { "LakeWood", "BridgeWood" };
+		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
+	}
 }
