85c3c9640655ff3f4532b259a947a4e4738de957
Mayur <mayurzope555@gmail.com>
2020-10-15 15:56:43 +0530

    [Mayur]Added: Cheapest Hotel for given date Range for Reward Customer Using Java Stream

--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -2,10 +2,10 @@ package com.bridgelabz.hotelreservation;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelReservation {
 	ArrayList<Hotel> hotelList;
@@ -30,59 +30,59 @@ public class HotelReservation {
 		hotelList.add(new Hotel("BridgeWood", 4, 150, 50, 110, 50));
 		hotelList.add(new Hotel("RidgeWood", 5, 220, 150, 100, 40));
 	}
-	
-	 /*Date Validation*/
-    private static boolean validateDate(String date) {
-        try {
-            LocalDate.parse(date);
-        } catch (DateTimeParseException e) {
-            return false;
-        }
-        return true;
-    }
-	
-	/* Method to validate and take the user input*/
-    public static void main(String[] args) throws Exception {
-        HotelReservation reservation = new HotelReservation();
-        Scanner sc = new Scanner(System.in);;
-        System.out.println("Welcome to Hotel Reservation System");
-        while (true) {
-            System.out.println("Enter the Hotel Name for Adding rates: ");
-            String hotelName = sc.next();
-            System.out.println("Enter Hotel Rating: ");
-            int rating = sc.nextInt();
-            System.out.println("Enter the Weekday rate: ");
-            int weeklyRate = sc.nextInt();
-            System.out.println("Enter the Weekend rate");
-            int weekEndRate = sc.nextInt();
-            System.out.println("Enter the Weekday rate for reward customer");
-            int weeklyRateReward = sc.nextInt();
-            System.out.println("Enter the Weekend rate for reward customer");
-            int weekEndRateReward = sc.nextInt();
-            reservation.addHotelDetails();
-            System.out.println("Add more hotel- (Yes/No)");
-            String choice = sc.next();
-            if (choice.equalsIgnoreCase("Yes"))
-                continue;
-            else
-                break;
-        }
-        System.out.println("Enter Arrival Date(DD/MM/YYYY):- ");
-        String arrivalDate = sc.next();
-        System.out.println("Enter Checkout Date(DD/MM/YYYY):- ");
-        String checkoutDate = sc.next();
-        if (!(validateDate(arrivalDate) && validateDate(checkoutDate)))
-            throw new Exception("Invalid date");
-        System.out.println("Enter the type of customer (Regular/Reward)");
-        String customer = sc.next();
-        if (customer.equalsIgnoreCase("Regular"))
-            reservation.findCheapestCostWithMaxRatings(arrivalDate, checkoutDate,false);
-        else if (customer.equalsIgnoreCase("Reward"))
-            reservation.findCheapestCostWithMaxRatings(arrivalDate, checkoutDate, true);
-        else
-            throw new Exception("Wrong Customer type");
-    }
 
+	/* Date Validation */
+	private static boolean validateDate(String date) {
+		try {
+			LocalDate.parse(date);
+		} catch (DateTimeParseException e) {
+			return false;
+		}
+		return true;
+	}
+
+	/* Method to validate and take the user input */
+	public static void main(String[] args) throws Exception {
+		HotelReservation reservation = new HotelReservation();
+		Scanner sc = new Scanner(System.in);
+		;
+		System.out.println("Welcome to Hotel Reservation System");
+		while (true) {
+			System.out.println("Enter the Hotel Name for Adding rates: ");
+			String hotelName = sc.next();
+			System.out.println("Enter Hotel Rating: ");
+			int rating = sc.nextInt();
+			System.out.println("Enter the Weekday rate: ");
+			int weeklyRate = sc.nextInt();
+			System.out.println("Enter the Weekend rate");
+			int weekEndRate = sc.nextInt();
+			System.out.println("Enter the Weekday rate for reward customer");
+			int weeklyRateReward = sc.nextInt();
+			System.out.println("Enter the Weekend rate for reward customer");
+			int weekEndRateReward = sc.nextInt();
+			reservation.addHotelDetails();
+			System.out.println("Add more hotel- (Yes/No)");
+			String choice = sc.next();
+			if (choice.equalsIgnoreCase("Yes"))
+				continue;
+			else
+				break;
+		}
+		System.out.println("Enter Arrival Date(DD/MM/YYYY):- ");
+		String arrivalDate = sc.next();
+		System.out.println("Enter Checkout Date(DD/MM/YYYY):- ");
+		String checkoutDate = sc.next();
+		if (!(validateDate(arrivalDate) && validateDate(checkoutDate)))
+			throw new Exception("Invalid date");
+		System.out.println("Enter the type of customer (Regular/Reward)");
+		String customer = sc.next();
+		if (customer.equalsIgnoreCase("Regular"))
+			reservation.findCheapestCostWithMaxRatings(arrivalDate, checkoutDate, false);
+		else if (customer.equalsIgnoreCase("Reward"))
+			reservation.findCheapestCostWithMaxRatings(arrivalDate, checkoutDate, true);
+		else
+			throw new Exception("Wrong Customer type");
+	}
 
 	/* count week days */
 	private static long countWeekDaysMath(LocalDate arrivalDay, LocalDate departureDay) {
@@ -99,13 +99,12 @@ public class HotelReservation {
 	}
 
 	/* Find hotel by maximum ratings */
-	public String calculateCheapestHotelAndRateByRating(String dateOfArrival, String dateOfDeparture) {
+	public String calculateCheapestHotelAndRateByRating(String dateOfArrival, String dateOfDeparture,
+			boolean isRewardee) {
 		long cost = 0;
 		rating = 0;
 		String hotelName = null;
-		ArrayList<String> hotel = new ArrayList<>();
-		ArrayList<Integer> eachHotelRate = calculateTotalCost(dateOfArrival, dateOfDeparture);
-		long minCostHotel = Math.min(eachHotelRate.get(0), Math.min(eachHotelRate.get(1), eachHotelRate.get(2)));
+		ArrayList<Integer> eachHotelRate = calculateTotalCost(dateOfArrival, dateOfDeparture, isRewardee);
 		for (int i = 0; i < hotelList.size(); i++) {
 			if ((hotelList.get(i).getHotelRating() > rating)) {
 				hotelName = hotelList.get(i).getHotelName();
@@ -119,23 +118,20 @@ public class HotelReservation {
 	}
 
 	/* Find cheapest hotel */
-	public ArrayList<String> calculateCheapestHotelAndRate(String dateOfArrival, String dateOfDeparture) {
-		ArrayList<String> hotel = new ArrayList<>();
-		String hotelName = null;
-		ArrayList<Integer> eachHotelRate = calculateTotalCost(dateOfArrival, dateOfDeparture);
-		long minCostHotel = Math.min(eachHotelRate.get(0), Math.min(eachHotelRate.get(1), eachHotelRate.get(2)));
-		if (minCostHotel == eachHotelRate.get(0))
-			hotel.add("LakeWood");
-		if (minCostHotel == eachHotelRate.get(1))
-			hotel.add("BridgeWood");
-		if (minCostHotel == eachHotelRate.get(2))
-			hotel.add("RidgeWood");
+	public ArrayList<String> calculateCheapestHotelAndRate(String dateOfArrival, String dateOfDeparture,
+			boolean isRewardee) {
+		int minCostHotel = hotelList.stream()
+				.map(hotel -> calculateTotalCost(hotel, dateOfArrival, dateOfDeparture, isRewardee))
+				.min((x, y) -> x - y).get();
+		List<String> cheapestHotelList = hotelList.stream()
+				.filter(hotel -> minCostHotel == calculateTotalCost(hotel, dateOfArrival, dateOfDeparture, isRewardee))
+				.map(hotel -> hotel.getHotelName()).collect(Collectors.toList());
 		System.out.println("The minimum cost for Hotel is " + minCostHotel);
-		return hotel;
+		return (ArrayList<String>) cheapestHotelList;
 	}
 
 	/* Calculate Total Cost of Each Hotel based on given date */
-	public ArrayList<Integer> calculateTotalCost(String dateOfArrival, String dateOfDeparture) {
+	public ArrayList<Integer> calculateTotalCost(String dateOfArrival, String dateOfDeparture, boolean isRewardee) {
 		LocalDate dateArrival = LocalDate.parse(dateOfArrival);
 		LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
 		totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
@@ -143,58 +139,42 @@ public class HotelReservation {
 		weekendDays = totalDays - weekDays;
 		ArrayList<Integer> eachHotelRate = new ArrayList<>();
 		for (Hotel hotel : hotelList) {
-			eachHotelRate.add((int) (hotel.getRegularRate() * weekDays + hotel.getWeekendRate() * weekendDays));
+			if (!isRewardee)
+				eachHotelRate.add((int) (hotel.getRegularRate() * weekDays + hotel.getWeekendRate() * weekendDays));
+			else
+				eachHotelRate.add((int) ((int) (hotel.getRewardWeekDayRate()) * weekDays
+						+ hotel.getRewardWeekendRate() * weekendDays));
 		}
 		return eachHotelRate;
 
 	}
 
-	public ArrayList<Integer> calculateTotalCostRewardCustomer(String dateOfArrival, String dateOfDeparture) {
+	/* Calculate Total Cost of Each Hotel based on given date */
+	public int calculateTotalCost(Hotel hotelName, String dateOfArrival, String dateOfDeparture, boolean isRewardee) {
 		LocalDate dateArrival = LocalDate.parse(dateOfArrival);
 		LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
 		totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
 		weekDays = countWeekDaysMath(dateArrival, dateDeparture);
 		weekendDays = totalDays - weekDays;
-		ArrayList<Integer> eachHotelRateRewardCustomer = new ArrayList<>();
-		for (Hotel hotel : hotelList) {
-			eachHotelRateRewardCustomer.add((int) ((int) (hotel.getRewardWeekDayRate()) * weekDays
-					+ hotel.getRewardWeekendRate() * weekendDays));
-		}
-		return eachHotelRateRewardCustomer;
-
+		if (isRewardee)
+			return (int) (hotelName.getRewardWeekDayRate() * weekDays + hotelName.getRewardWeekendRate() * weekendDays);
+		return (int) (hotelName.getRegularRate() * weekDays + hotelName.getWeekendRate() * weekendDays);
 	}
 
-	/* UC6 Find cheapest hotel with maximum rating */
+	/*  Find cheapest hotel with maximum rating */
 	public String findCheapestCostWithMaxRatings(String dateOfArrival, String dateOfDeparture,
 			boolean isRewardCustomer) {
-		String hotelName = null;
-		if (isRewardCustomer == false) {
-			int rating = 0;
-			ArrayList<Integer> eachHotelRate = calculateTotalCost(dateOfArrival, dateOfDeparture);
-			long minCostHotel = Math.min(eachHotelRate.get(0), Math.min(eachHotelRate.get(1), eachHotelRate.get(2)));
-			for (int i = 0; i < hotelList.size(); i++) {
-				if (minCostHotel == eachHotelRate.get(i) && (hotelList.get(i).getHotelRating() > rating)) {
-					rating = hotelList.get(i).getHotelRating();
-					hotelName = hotelList.get(i).getHotelName();
-				}
-			}
-			System.out.println("The minimum cost for Hotel " + hotelName + " is " + minCostHotel
-					+ " and rating of hotel is " + rating);
-		} else {
-			int rating = 0;
-			ArrayList<Integer> eachHotelRateRewardCustomer = calculateTotalCostRewardCustomer(dateOfArrival, dateOfDeparture);
-			long minCostHotel = Math.min(eachHotelRateRewardCustomer.get(0),
-					Math.min(eachHotelRateRewardCustomer.get(1), eachHotelRateRewardCustomer.get(2)));
-			for (int i = 0; i < hotelList.size(); i++) {
-				if (minCostHotel == eachHotelRateRewardCustomer.get(i)
-						&& (hotelList.get(i).getHotelRating() > rating)) {
-					rating = hotelList.get(i).getHotelRating();
-					hotelName = hotelList.get(i).getHotelName();
-				}
-			}
-			System.out.println("The minimum cost for Hotel " + hotelName + "for Reward Customer is " + minCostHotel
-					+ " and rating of hotel is " + rating);
-		}
-		return hotelName;
+		int minCostHotel = hotelList.stream()
+				.map(hotel -> calculateTotalCost(hotel, dateOfArrival, dateOfDeparture, isRewardCustomer))
+				.min((x, y) -> x - y).get();
+		List<Hotel> cheapestHotelList = hotelList.stream().filter(
+				hotel -> minCostHotel == calculateTotalCost(hotel, dateOfArrival, dateOfDeparture, isRewardCustomer))
+				.collect(Collectors.toList());
+		Hotel h = cheapestHotelList.stream().max((x, y) -> x.getHotelRating() - y.getHotelRating()).get();
+		String cheapestHotelName = h.getHotelName();
+		System.out.println("The minimum cost for Hotel " + cheapestHotelName + " is " + minCostHotel
+				+ " and rating of hotel is " + h.getHotelRating());
+		return cheapestHotelName;
 	}
+
 }
--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -8,23 +8,18 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class HotelReservationTest {
-	static HotelReservation hotelReservation;
+	static HotelReservation hotelReservation = new HotelReservation();
 
 	@BeforeClass
 	public static void initialize() {
-		hotelReservation = new HotelReservation();
-		hotelReservation.printWelcomeMessage();
-	}
-
-	@Before
-	public void shouldPrintWelcomeMessage() {
 		hotelReservation.addHotelDetails();
+		hotelReservation.printWelcomeMessage();
 	}
 
 	/* UC2 Cheapest Hotel with weekday */
 	@Test
 	public void givenDate_shouldReturn_CheapestHotelNameAndRate() throws Exception {
-		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-10", "2020-09-11");
+		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-10", "2020-09-11",false);
 		Object[] hotelName = hotelNameList.toArray();
 		Object[] arrayExpectedOutput = { "LakeWood", "BridgeWood" };
 		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
@@ -33,16 +28,17 @@ public class HotelReservationTest {
 	/* UC2 Cheapest Hotel with weekday weekend */
 	@Test
 	public void givenDate_shouldReturn_CheapestHotelNameLakeWoodAndBridgeWood() throws Exception {
-		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-11", "2020-09-12");
-		Object[] hotelName = hotelNameList.toArray();
-		Object[] arrayExpectedOutput = { "LakeWood", "BridgeWood" };
-		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
+		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-11", "2020-09-12",false);
+		ArrayList<String> expectedHotelList = new ArrayList<>();
+		expectedHotelList.add("LakeWood");
+		expectedHotelList.add("BridgeWood");
+		Assert.assertEquals(expectedHotelList, hotelNameList);
 	}
 
 	/* UC2 Cheapest Hotel with weekend days */
 	@Test
 	public void givenDateWeekend_shouldReturn_CheapestHotelNameBridgeWood() throws Exception {
-		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-12", "2020-09-13");
+		ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-12", "2020-09-13",false);
 		Object[] hotelName = hotelNameList.toArray();
 		Object[] arrayExpectedOutput = { "BridgeWood" };
 		Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
@@ -72,21 +68,21 @@ public class HotelReservationTest {
 	/* UC7 Cost with maximum Ratings weekday */
 	@Test
 	public void givenDate_WeekDayshouldReturn_CheapestHotelNameRidgeWood() throws Exception {
-		String hotelName = hotelReservation.calculateCheapestHotelAndRateByRating("2020-09-14", "2020-09-16");
+		String hotelName = hotelReservation.calculateCheapestHotelAndRateByRating("2020-09-14", "2020-09-16",false);
 		Assert.assertEquals("RidgeWood", hotelName);
 	}
 
 	/* UC7 Cost with maximum Ratings weekend */
 	@Test
 	public void givenDateWeekend_shouldReturn_CheapestHotelNameRidgeeWood() throws Exception {
-		String hotelName = hotelReservation.calculateCheapestHotelAndRateByRating("2020-09-12", "2020-09-13");
+		String hotelName = hotelReservation.calculateCheapestHotelAndRateByRating("2020-09-12", "2020-09-13",false);
 		Assert.assertEquals("RidgeWood", hotelName);
 	}
 
 	/* UC7 Cost with maximum Ratings weekday weekend */
 	@Test
 	public void givenDateWeekDayWeekend_shouldReturn_CheapestHotelNameGivenBridgeWood() throws Exception {
-		String hotelName = hotelReservation.calculateCheapestHotelAndRateByRating("2020-09-11", "2020-09-12");
+		String hotelName = hotelReservation.calculateCheapestHotelAndRateByRating("2020-09-11", "2020-09-12",false);
 		Assert.assertEquals("RidgeWood", hotelName);
 	}
 	
